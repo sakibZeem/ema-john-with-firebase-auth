f6d824d5d6b9971429f12001922b9e24955e247f
sakibZeem <anamulsakibzim22@gmail.com>
2024-01-07 22:12:58 +0600

    firebase configuration added and signup route created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqTo3SytKYBlDko_ClCkl_b1ECb751XMM",
+  authDomain: "ema-john-with-firebase-a-3977a.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-3977a",
+  storageBucket: "ema-john-with-firebase-a-3977a.appspot.com",
+  messagingSenderId: "60006933430",
+  appId: "1:60006933430:web:2359fd8a6cf404c385d717"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
